9ead244636615d257371864ca3c7572ec65c6567
quadrodaddy <93032361+quadrodaddy@users.noreply.github.com>
2022-05-19 15:17:05 +0300

    Доработаны замечания от 18.05.2022. В классе ArrayStorage добавлено поле size, изменены методы.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,26 +6,29 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0; // добавил поле для определения количества резюме
 
     void clear() {
-        Arrays.fill(storage, null); // очищаем массив путем замены всех резюме на null
+        Arrays.fill(storage, 0, size, null); // исправил метод fill
+
     }
 
     void save(Resume r) {
-        if (r != null && !r.uuid.isEmpty()) { // если резюме существует и uuid резюме не пусто
-            for (int i = 0; i <= storage.length - 1; i++) { // то проходим циклом по массиву
-                if (storage[i] == null) { //нашли пустую ячейку
-                    storage[i] = r; // сохраняем в нее резюме
-                    break;  // прекращаем цикл, чтоб не проверять все 10000
+        if (r != null && !r.uuid.isEmpty()) {
+            for (int i = 0; i <= storage.length - 1; i++) {
+                if (storage[i] == null) {
+                    storage[i] = r;
+                    size++; //добавил инкремент
+                    break;
                 }
             }
         }
     }
 
     Resume get(String uuid) {
-        if (uuid != null && !uuid.isEmpty()) { // если uuid корректный
-            for (int i = 0; i <= storage.length - 1; i++) { // то проходим циклом по массиву
-                if (storage[i] != null && uuid.equalsIgnoreCase(storage[i].uuid)) { // если находим в массиве uuid, соответствующий искомому
+        if (uuid != null && !uuid.isEmpty()) {
+            for (int i = 0; i <= size - 1; i++) { // исправил на size, чтоб весь массив не проходить
+                if (uuid.equalsIgnoreCase(storage[i].uuid)) { //убрал проверку if storage[i] != null
                     return storage[i];
                 }
             }
@@ -34,10 +37,12 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        if (uuid != null) { // если резюме существует
-            for (int i = 0; i <= storage.length - 1; i++) { // то проходим циклом по массиву
-                if (storage[i] != null && uuid.equalsIgnoreCase(storage[i].uuid)) { // если находим в массиве uuid, соответствующий искомому
-                    storage[i] = null; // удаляем его
+        if (uuid != null) {
+            for (int i = 0; i <= size - 1; i++) { // исправил на size,
+                if (uuid.equalsIgnoreCase(storage[i].uuid)) { //убрал проверку if storage[i] != null
+                    storage[i] = null;
+                    storage[size + 1] = storage[i]; // переместил пустое рюзюме в следующую ячейку за базой резюме
+                    size--;
                 }
             }
         }
@@ -46,25 +51,13 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        Resume[] fillStorage = new Resume[size()]; //создали копию массива, размер которого зависит от кол-ва резюме в базе
-        int indexFS = 0; //создали счетчик для подсчета резюме
-        for (int i = 0; i <= storage.length - 1; i++) { //циклом прошли по массиву
-            if (storage[i] != null) { //если находит резюме,
-                fillStorage[indexFS] = storage[i]; // то в элемент нового массива присваивается резюме из storage
-                indexFS++; // увеличиваем элемент нового массива и ищем для него новое резюме
-            }
-        }
-        return fillStorage; // возвращаем новый массив, состоящий только из резюме (без null)
+    Resume[] getAll() { //убрал цикл из метода
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
-    int size() {
-        int count = 0;//создали счетчик размера заполненного массива
-        for (int i = 0; i <= storage.length - 1; i++) { //циклом прошли по массиву
-            if (storage[i] != null) { //если находит резюме,
-                count++; // то счетчик увеличиваем на 1
-            }
-        }
-        return count; //возвращаем количество резюме в массиве
+    int size() { // убрал цикл, привел к примитиву, добавив невозможность уйти в минус
+        if (size >= 0)
+            return size;
+        else return 0;
     }
 }
